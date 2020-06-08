d75e88c3ee630ed917ed86803a3098d2b0711d1d
Oleksii Lubynets <lubynets@lxbk0195.gsi.de>
2020-06-08 15:47:25 +0200

    recover passcuts var

--- Interface/InputContainer.h
+++ Interface/InputContainer.h
@@ -42,7 +42,7 @@ class InputContainer{
   void SetPV(float x, float y, float z);
   void SetPV(KFVertex vertex);
   void SetPV(KFPVertex vertex);
-  void AddTrack(const std::vector<float>& par, const std::vector<float>& cov, const std::vector<float>& field, int charge, int pdg, int id, int passcuts);
+  void AddTrack(const std::vector<float>& par, const std::vector<float>& cov, const std::vector<float>& field, int charge, int pdg, int id, int passcuts=1);
 //   KFParticleTopoReconstructor* CreateTopoReconstructor();                                                                                                   //^ not good
 
 
