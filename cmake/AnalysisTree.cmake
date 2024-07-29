f812ce384e93f3a27e648d275e0130fcf71999df
Oleksii Lubynets <lubinets95@gmail.com>
2024-07-29 22:10:48 +0200

    improve auto-install of AT; add config.sh

--- cmake/AnalysisTree.cmake
+++ cmake/AnalysisTree.cmake
@@ -1,10 +1,10 @@
-if (PFSimple_BUNDLED_AT)
+if(PFSimple_BUNDLED_AT)
     message("-- Building bundled AnalysisTree")
     include(FetchContent)
 
     FetchContent_Declare(
             AnalysisTree
-            GIT_REPOSITORY "https://github.com/HeavyIonAnalysis/AnalysisTree.git"
+            GIT_REPOSITORY ${PFSimple_BUNDLED_AT_URL}
             GIT_TAG ${PFSimple_BUNDLED_AT_VERSION}
             GIT_SHALLOW ${PFSimple_BUNDLED_AT_GIT_SHALLOW}
     )
@@ -20,6 +20,7 @@ else()
         list(APPEND PROJECT_INCLUDE_DIRECTORIES ${AnalysisTree_INCLUDE_DIR})
         list(APPEND PROJECT_LINK_LIBRARIES AnalysisTreeBase AnalysisTreeInfra)
     else()
-        message(WARNING "AnalysisTree is not found, AT interface will not be compiled")
+        message(FATAL_ERROR "Set AnalysisTree_DIR to a directory containing AnalysisTreeConfig.cmake file\n"
+                            "OR\nuse -DPFSimple_BUNDLED_AT=ON option to build AnalysisTree automatically")
     endif()
 endif()
