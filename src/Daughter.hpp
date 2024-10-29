d879a16a5c5cf7b06cd75fc0d1425bb59882fb15
SusanneGlaessel <glaessel@ikf.uni-frankfurt.de>
2024-10-29 19:30:01 +0100

    clang format

--- src/Daughter.hpp
+++ src/Daughter.hpp
@@ -34,12 +34,12 @@ class Daughter {
   void CancelCuts();
 
  protected:
-  Pdg_t pdg_hypo_{-1};         ///< PDG code hypothesis
-  std::vector<Pdg_t> pids_{};  ///< vector of PDG codes to use
-  float chi2_prim_{18.4207};   ///< \f$\chi^2\f$ lower value
-  //float cos_{0.f};             ///< cosine lower value
-  float cos_{  -huge_value};  ///< cosine lower value   
-  int id_{-1};                 ///< daughther number (0, 1, 2)
+  Pdg_t pdg_hypo_{-1};       ///< PDG code hypothesis
+  std::vector<Pdg_t> pids_{};///< vector of PDG codes to use
+  float chi2_prim_{18.4207}; ///< \f$\chi^2\f$ lower value
+  //float cos_{0.f};           ///< cosine lower value
+  float cos_{  -huge_value}; ///< cosine lower value   
+  int id_{-1};               ///< daughther number (0, 1, 2)
 };
 
 #endif//KFPARTICLESIMPLE_KFSIMPLE_DAUGHTERCUTS_HPP_
