370936a521da4b55d6fa54a4a3840717ed4ab42c
SusanneGlaessel <glaessel@ikf.uni-frankfurt.de>
2024-11-22 14:54:45 +0100

    clang

--- src/SimpleFinder.cpp
+++ src/SimpleFinder.cpp
@@ -378,11 +378,11 @@ void SimpleFinder::ReconstructDecay(const Decay& decay) {
         for (auto index_3 : indexes.at(2)) {
 
           track.at(2) = GetTrack(index_3);
-          if (std::abs(decay.GetDaughters().at(2).GetPdgHypo()) == 1000020030 || std::abs(decay.GetDaughters().at(2).GetPdgHypo()) == 1000020040) {
-            int charge = (int) track.at(2).Q();
-            charge *= 2;
-            track.at(2).Q() = charge;
-            track.at(2).Px() *= std::abs(charge);
+          if (std::abs(decay.GetDaughters().at(2).GetPdgHypo()) == 1000020030 || std::abs(decay.GetDaughters().at(2).GetPdgHypo()) == 1000{
+	    int charge = (int) track.at(2).Q();
+	    charge *= 2;
+	    track.at(2).Q() = charge;
+	    track.at(2).Px() *= std::abs(charge);
 	    track.at(2).Py() *= std::abs(charge);
 	    track.at(2).Pz() *= std::abs(charge);
 	  }
